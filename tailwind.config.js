ffecabaa489be14ef1a24ed23942d7061a608967
paksiswanto <unitedkuryu@gmail.com>
2026-01-19 16:10:33 +0700

    add another package

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Membaca semua file .html di folder utama
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
