6faecefe9869e9b5f4936a78650166aecdc9f503
paksiswanto <unitedkuryu@gmail.com>
2026-02-19 09:45:38 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,44 +1,46 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
 (function ($) {
     "use strict";
 
     // Spinner
     var spinner = function () {
         setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
+            if ($("#spinner").length > 0) {
+                $("#spinner").removeClass("show");
             }
         }, 1);
     };
     spinner();
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
-
 
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 45) {
-            $('.nav-bar').addClass('sticky-top');
+            $(".nav-bar").addClass("sticky-top");
         } else {
-            $('.nav-bar').removeClass('sticky-top');
+            $(".nav-bar").removeClass("sticky-top");
         }
     });
-    
-    
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
+            $(".back-to-top").fadeIn("slow");
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $(".back-to-top").fadeOut("slow");
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    $(".back-to-top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
         return false;
     });
-
 
     // Header carousel
     $(".header-carousel").owlCarousel({
@@ -47,13 +49,9 @@
         items: 1,
         dots: true,
         loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
+        nav: true,
+        navText: ['<i class="bi bi-chevron-left"></i>', '<i class="bi bi-chevron-right"></i>'],
     });
-
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
@@ -62,20 +60,16 @@
         margin: 24,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
+        nav: true,
+        navText: ['<i class="bi bi-arrow-left"></i>', '<i class="bi bi-arrow-right"></i>'],
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1,
             },
-            992:{
-                items:2
-            }
-        }
+            992: {
+                items: 2,
+            },
+        },
     });
-    
 })(jQuery);
 
